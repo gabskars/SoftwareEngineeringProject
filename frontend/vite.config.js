37a7d5983106c1065d8682227a1e5727c8c9006f
Gabriela Ximenes <gabigxx1@gmail.com>
2025-07-17 09:20:40 -0300

    Corrige conteúdo do vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/SoftwareEngineeringProject/',
-=======
   base: "/",
->>>>>>> 6350c01f401af37a19d4cea730f8df1e2d485e56
   plugins: [react()],
 });
