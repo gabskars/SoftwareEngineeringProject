6f1979a7b3d54eb13e06e87412c30d8fe49fc82a
Gabriela Ximenes <gabigxx1@gmail.com>
2025-07-17 09:12:41 -0300

    Minhas alterações locais no vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SoftwareEngineeringProject/', 
+  base: '/SoftwareEngineeringProject/',
   plugins: [react()],
 })
